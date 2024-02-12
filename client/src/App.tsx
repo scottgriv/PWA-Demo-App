bee6b80a7a88948a05d78713495c9f487cee1f3b
Scott Griv <scottgrivner@gmail.com>
2024-02-12 15:24:20 -0500

    Update title

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
           <a href="https://en.wikipedia.org/wiki/Progressive_web_app" target="_blank" rel="noreferrer">
             <img src={`${process.env.PUBLIC_URL}/icons/logo512.png`} alt="Logo" className="App-logo" />
           </a>
-          <h1>Welcome to the PWA Demo</h1>
+          <h1>PWA Demo Application</h1>
         </div>
         <p>This is a demo PWA using React, TypeScript, Node.js, Postgresql, and Scss/Sass/Less.<br /><a href="https://github.com/scottgriv/pwa-demo_app" target="_blank" rel="noreferrer">Visit the repository on GitHub</a> for more information on how to build your own PWA website.</p>
       </div>
